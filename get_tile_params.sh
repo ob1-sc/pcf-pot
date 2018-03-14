767e9a7f274999c58b2712fd66fb638858d1447a
Simon O'Brien <simon@the-obriens.net>
2018-03-14 11:47:14 +0000

    Added additional check

--- get_tile_params.sh
+++ get_tile_params.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-while getopts ":u" opt; do
+while getopts ":u:" opt; do
   case $opt in
     a)
-      echo "-u was triggered!" >&2
+      echo "-u was triggered, Parameter: $OPTARG" >&2
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
       ;;
   esac
 done
