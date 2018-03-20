4b225ddde728431193dcf784d1ee930424f64a02
Simon O'Brien <simon@the-obriens.net>
2018-03-20 09:22:31 +0000

    able to get oauth token

--- tile-params/get_params.py
+++ tile-params/get_params.py
@@ -1,34 +1,36 @@
 import argparse
+import requests
 
 # setup an argument parser
-parser = argparse.ArgumentParser(description='Tile Param Checker')
+from requests.auth import HTTPBasicAuth
 
-# define the program arguments
-parser.add_argument("om_url")
-parser.add_argument("username")
-parser.add_argument("password")
 
-# parse the arguments
-args = parser.parse_args()
+def login(om_url, username, password, verify_ssl):
 
-# define the uaa login url
-uaa_login_url = args.om_url + "/uaa/login"
-
-def login(username, password):
-
-    oauthTokenResponse = requests.post(
-        "https://login." + foundationUrl + "/oauth/token?grant_type=password&client_id=cf",
-        data={"username": username, "password": password, 'client_id': "cf"},
-        auth=("cf", ""),
-        verify=skipSsl
+    oauth_token_response = requests.post(
+        "https://" + om_url + "/uaa/oauth/token?grant_type=password",
+        auth=HTTPBasicAuth("opsman", ""),
+        data={"username": username, "password": password},
+        verify=verify_ssl
     )
 
-    return oauthTokenResponse.json()["token_type"] + " " + oauthTokenResponse.json()["access_token"]
+    return oauth_token_response.json()["token_type"] + " " + oauth_token_response.json()["access_token"]
 
 
-########################### MAIN ############################
+def main():
 
-# login
-authorization = login("admin", "admin")
+    parser = argparse.ArgumentParser(description='Tile Param Checker')
 
-print authorization
+    # define the program arguments
+    parser.add_argument("om_url")
+    parser.add_argument("username")
+    parser.add_argument("password")
+    parser.add_argument("--skip-ssl-validation", action="store_false", dest="verify_ssl")
+
+    # parse the arguments
+    args = parser.parse_args()
+
+    # login
+    authorization = login(args.om_url, args.username, args.password, args.verify_ssl)
+
+    print authorization
